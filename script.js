5e6a2f51002ad29aa6155bf22656120cdcb4af0a
Sonwani_Dev <sonwani.dev22@gmail.com>
2023-10-04 23:10:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,311 @@
+const slider1 = document.querySelectorAll(".slide1");
+slider1.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide1 = () => {
+  slider1.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide2 = () => {
+  slider1.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide3 = () => {
+  slider1.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide4 = () => {
+  slider1.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider2 = document.querySelectorAll(".slide2");
+slider2.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide5 = () => {
+  slider2.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide6 = () => {
+  slider2.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide7 = () => {
+  slider2.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide8 = () => {
+  slider2.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider3 = document.querySelectorAll(".slide3");
+slider3.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide9 = () => {
+  slider3.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide10 = () => {
+  slider3.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide11 = () => {
+  slider3.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide12 = () => {
+  slider3.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider4 = document.querySelectorAll(".slide4");
+slider4.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide13 = () => {
+  slider4.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide14 = () => {
+  slider4.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide15 = () => {
+  slider4.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide16 = () => {
+  slider4.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider5 = document.querySelectorAll(".slide5");
+slider5.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide17 = () => {
+  slider5.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide18 = () => {
+  slider5.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide19 = () => {
+  slider5.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide20 = () => {
+  slider5.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider6 = document.querySelectorAll(".slide6");
+slider6.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide21 = () => {
+  slider6.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide22 = () => {
+  slider6.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide23 = () => {
+  slider6.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide24 = () => {
+  slider6.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider7 = document.querySelectorAll(".slide7");
+slider7.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide25 = () => {
+  slider7.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide26 = () => {
+  slider7.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide27 = () => {
+  slider7.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide28 = () => {
+  slider7.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider8 = document.querySelectorAll(".slide8");
+slider8.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide29 = () => {
+  slider8.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide30 = () => {
+  slider8.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide31 = () => {
+  slider8.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide32 = () => {
+  slider8.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider9 = document.querySelectorAll(".slide9");
+slider9.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide33 = () => {
+  slider9.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide34 = () => {
+  slider9.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide35 = () => {
+  slider9.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide36 = () => {
+  slider9.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider10 = document.querySelectorAll(".slide10");
+slider10.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide37 = () => {
+  slider10.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide38 = () => {
+  slider10.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide39 = () => {
+  slider10.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide40 = () => {
+  slider10.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider11 = document.querySelectorAll(".slide11");
+slider11.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide41 = () => {
+  slider11.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide42 = () => {
+  slider11.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide43 = () => {
+  slider11.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide44 = () => {
+  slider11.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
+
+const slider12 = document.querySelectorAll(".slide12");
+slider12.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+const slide45 = () => {
+  slider12.forEach((slide) => {
+    slide.style.transform = `translateX(0%)`;
+  });
+};
+const slide46 = () => {
+  slider12.forEach((slide) => {
+    slide.style.transform = `translateX(-100%)`;
+  });
+};
+const slide47 = () => {
+  slider12.forEach((slide) => {
+    slide.style.transform = `translateX(-200%)`;
+  });
+};
+const slide48 = () => {
+  slider12.forEach((slide) => {
+    slide.style.transform = `translateX(-300%)`;
+  });
+};
